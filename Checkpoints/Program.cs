76c124117a27d515fe7b66de863554c457ccefeb
Chris B. Behrens <cbehrens@gmail.com>
2022-04-15 15:22:26 -0500

    Before solution

--- Checkpoints/Program.cs
+++ Checkpoints/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+Console.Write("Please enter your number:");
+var number = Console.ReadLine();
+
+int x;
+
+while(!int.TryParse(number, out x))
+{
+    Console.WriteLine("Please enter an integer value:");
+    number = Console.ReadLine();
+}
+
+var square = SquareNumber(x);
+
+Console.WriteLine($"The square of {x} is {square}");
+
+int SquareNumber(int value)
+{
+    return value * value;
+}

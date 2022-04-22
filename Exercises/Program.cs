162ce12c5284ebdf64b274239828c7a7e3dc9af7
Chris B. Behrens <cbehrens@gmail.com>
2022-04-22 14:15:15 -0500

    Removed solution

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -12,14 +12,11 @@ while (!int.TryParse(entry, out x))
     entry = Console.ReadLine();
 }
 
-Console.WriteLine("TODO: Implement func");
-return;
-
-/*ConstantExpression xParameter = Expression.Constant(x);
+ConstantExpression xParameter = Expression.Constant(x);
 ConstantExpression twelve = Expression.Constant(12);
 Expression greaterThan = Expression.GreaterThan(xParameter, twelve);
 
 var expr = Expression.Lambda<Func<bool>>(greaterThan);
 
 var func = expr.Compile();
-Console.WriteLine(x.ToString() + " is " + (func() ? "" : "not ") + "greater than 12");*/
+Console.WriteLine(x.ToString() + " is " + (func() ? "" : "not ") + "greater than 12");

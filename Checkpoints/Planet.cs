9525966580ff030bbcf8a0c37013b4ac291a0f70
Chris B. Behrens <cbehrens@gmail.com>
2022-04-22 14:52:57 -0500

    Checkpoint 03

---
+++ Checkpoints/Planet.cs
@@ -0,0 +1,20 @@
+﻿namespace Checkpoints
+{
+    public class Planet
+    {
+        /// <summary>
+        /// 0 = Rocky, 1 = Gas giant
+        /// </summary>
+        public int PlanetType
+        {
+            get;set;
+        }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Distance from the sun in AU
+        /// </summary>
+        public decimal StellarDistance { get; set;}
+    }
+}
--- Checkpoints/Program.cs
+++ Checkpoints/Program.cs
@@ -1,3 +1,31 @@
-﻿using System.Linq.Expressions;
+﻿using Checkpoints;
+using System.Linq.Expressions;
 
-Console.WriteLine("TODO: Create the expression");
+var planets = new List<Planet>
+{
+    new Planet {Name = "Mercury", PlanetType = 0, StellarDistance = .39m},
+    new Planet {Name = "Venus", PlanetType = 0, StellarDistance = .72m},
+    new Planet {Name = "Earth", PlanetType = 0, StellarDistance = 1m},
+    new Planet {Name = "Mars", PlanetType = 0, StellarDistance = 1.52m},
+    new Planet {Name = "Jupiter", PlanetType = 0, StellarDistance = 5.2m},
+    new Planet {Name = "Saturn", PlanetType = 0, StellarDistance = 9.54m},
+    new Planet {Name = "Uranus", PlanetType = 0, StellarDistance = 19.2m},
+    new Planet {Name = "Neptune", PlanetType = 0, StellarDistance = 30.06m}
+};
+
+var planetParam = Expression.Parameter(typeof(Planet), "planet");
+
+var OneAU = Expression.Constant(1m);
+
+var lessThan = Expression.LessThan(stellarDistance, OneAU);
+
+var expr = Expression.Lambda<Func<Planet, bool>>(lessThan, false, planetParam);
+
+var func = expr.Compile();
+
+foreach(var planet in planets.Where(func))
+{
+    Console.WriteLine(planet.Name);
+}
+
+Console.WriteLine("End of line");

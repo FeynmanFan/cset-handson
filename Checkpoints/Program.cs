134e4bfbf0205710629ac635a30d86dbe362d049
Chris B. Behrens <cbehrens@gmail.com>
2022-04-22 14:20:13 -0500

    Cleared deck for checkpoint 2

--- Checkpoints/Program.cs
+++ Checkpoints/Program.cs
@@ -1,21 +1,3 @@
-﻿using System.Linq;
+﻿using System.Linq.Expressions;
 
-Console.Write("Please enter your number:");
-var number = Console.ReadLine();
-
-int x;
-
-while(!int.TryParse(number, out x))
-{
-    Console.WriteLine("Please enter an integer value:");
-    number = Console.ReadLine();
-}
-
-Func<int, int> squareNumber = value => value * value; //SquareNumber(x);
-
-Console.WriteLine($"The square of {x} is {squareNumber(x)}");
-
-int SquareNumber(int value)
-{
-    return value * value;
-}
+Console.WriteLine("TODO: Create the expression");
